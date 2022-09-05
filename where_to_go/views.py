e48d141d2a58647c062e8de15432ed30fbb8d49c
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-09-05 20:26:15 +0300

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,9 +1,36 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 
 def show_mainpage(request):
-    template = loader.get_template('index.html')
-    context = {}
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)
+    context = {
+        'places': {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [37.62, 55.793676]
+                    },
+                    "properties": {
+                        "title": "«Легенды Москвы",
+                        "placeId": "moscow_legends",
+                        "detailsUrl": "../static/places/moscow_legends.json"
+                    }
+                },
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [37.64, 55.753676]
+                    },
+                    "properties": {
+                        "title": "Крыши24.рф",
+                        "placeId": "roofs24",
+                        "detailsUrl": "../static/places/roofs24.json"
+                    }
+                }
+            ]
+        }
+    }
+    return render(request, 'index.html', context)
