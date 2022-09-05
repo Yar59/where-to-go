86f06bc6552a0582fff6ba9f0e6f51d5e595c5c4
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-09-05 18:30:04 +0300

    Add and register model for images

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, Image
+from django.utils.safestring import mark_safe
 
-admin.site.register(Place)
+
+@admin.register(Place)
+class PlasceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    fields = (
+        'image',
+        'preview',
+        'place',
+    )
+    raw_id_fields = ['place',]
+    readonly_fields = ["preview",]
+
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 200px;">')
--- places/models.py
+++ places/models.py
@@ -10,3 +10,20 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField(
+        'Картинки',
+        null=True,
+        blank=True,
+    )
+    place = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        verbose_name='Место',
+        related_name='images'
+    )
+
+    def __str__(self):
+        return f'{self.id} {self.place.title} '
