f26f305c5cf19be86f7ab7ff7fb59415b782d617
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-09-06 14:51:24 +0300

    make images sortable

--- places/admin.py
+++ places/admin.py
@@ -1,23 +1,26 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
+from adminsortable2.admin import SortableStackedInline, SortableAdminBase
+
 from .models import Place, Image
 
 
-class ImageInline(admin.TabularInline):
+class ImageInline(SortableStackedInline):
     model = Image
     fields = (
         'image',
         'preview',
-        'ordinal_number',
+        'ordinal_number'
     )
     readonly_fields = ["preview", ]
+
     def preview(self, obj):
         return format_html(f'<img src="{obj.image.url}" style="max-height: 200px;">')
 
 
 @admin.register(Place)
-class PlasceAdmin(admin.ModelAdmin):
+class PlasceAdmin(SortableAdminBase, admin.ModelAdmin):
     inlines = [ImageInline, ]
 
 
