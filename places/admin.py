80d6071c061f410e81d8e5e340470f671360f611
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-09-06 14:22:28 +0300

    ad preview to ImageInline

--- places/admin.py
+++ places/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    fields = (
+        'image',
+        'preview',
+        'place',
+    )
+    readonly_fields = ["preview", ]
+    def preview(self, obj):
+        return format_html(f'<img src="{obj.image.url}" style="max-height: 200px;">')
 
 
 @admin.register(Place)
@@ -24,5 +32,5 @@ class ImageAdmin(admin.ModelAdmin):
     readonly_fields = ["preview",]
 
     def preview(self, obj):
-        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 200px;">')
+        return format_html(f'<img src="{obj.image.url}" style="max-height: 200px;">')
 
