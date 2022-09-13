cd42cb2d44a6eb5f9c94aa20cf0bbcb1b2b18345
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-09-13 11:15:44 +0300

    Update urls.py

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -22,7 +22,7 @@ from where_to_go import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.show_mainpage),
+    path('', views.show_main_page),
     path('places/<int:place_id>', views.fetch_place_details)
 ]
 
