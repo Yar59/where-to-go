6a28df5ffe0739d4b93611c3eb3b79370fa128e0
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-09-13 14:29:59 +0300

    removed indents from endpoint API response

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -44,4 +44,4 @@ def fetch_place_details(request, place_id):
             "lat": place.latitude,
         }
     }
-    return JsonResponse(payload, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 4})
+    return JsonResponse(payload, safe=False, json_dumps_params={'ensure_ascii': False})
