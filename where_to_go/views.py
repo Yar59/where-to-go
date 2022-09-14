1e83c24739b7f8887e1c849eace85e2ca625e7f2
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-09-14 16:20:39 +0300

    moved the code to a variable

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -7,21 +7,22 @@ from django.urls import reverse
 
 def show_main_page(request):
     places = Place.objects.all()
+    features = [{
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [place.longitude, place.latitude]
+                    },
+                    "properties": {
+                        "title": place.title,
+                        "placeId": place.id,
+                        "detailsUrl": reverse(fetch_place_details, kwargs={'place_id': place.id})
+                    }
+                    } for place in places]
     context = {
         'places': {
             "type": "FeatureCollection",
-            "features": [{
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [place.longitude, place.latitude]
-                },
-                "properties": {
-                    "title": place.title,
-                    "placeId": place.id,
-                    "detailsUrl": reverse(fetch_place_details, kwargs={'place_id': place.id})
-                }
-            } for place in places]
+            "features": features
         }
     }
     return render(request, 'index.html', context)
